c13a639c5c330ec253e9573e0041c9aa21b41b1b
Stela Dimitrova <stella.chilikova@gmail.com>
2022-10-02 22:49:03 +0300

    add new lab

---
+++ DataTypesAndVariables/Lab/p04TownInfo.java
@@ -0,0 +1,19 @@
+package DataTypesAndVariables.Lab;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class p04TownInfo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String town = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        short area = Short.parseShort(scanner.nextLine());
+
+        String result = String.format("Town %s has population of %d and area %d square km.", town, population, area);
+        System.out.println(result);
+
+    }
+}
