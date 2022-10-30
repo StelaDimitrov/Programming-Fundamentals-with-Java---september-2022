4c002a69cb7048d877fcbf0176f1045e58f9cadd
Stela Dimitrova <stella.chilikova@gmail.com>
2022-10-30 22:27:29 +0200

    add new

---
+++ ObjectsAndClasses/Lab/p03BigFactorial.java
@@ -0,0 +1,25 @@
+package ObjectsAndClasses.Lab;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class p03BigFactorial {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        BigInteger resultBigNum = new BigInteger("1");
+
+        for (int i = 1; i <= n; i++) {
+
+            resultBigNum = resultBigNum.multiply(BigInteger.valueOf(i));
+
+
+
+        }
+         System.out.println(resultBigNum);
+    }
+}
