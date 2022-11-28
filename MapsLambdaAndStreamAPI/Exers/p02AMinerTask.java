ed17d7d02bd612305ac9789b3ae19fdb0213b626
Stela Dimitrova <stella.chilikova@gmail.com>
2022-11-28 16:21:59 +0200

    more

---
+++ MapsLambdaAndStreamAPI/Exers/p02AMinerTask.java
@@ -0,0 +1,12 @@
+package MapsLambdaAndStreamAPI.Exers;
+
+import java.util.Scanner;
+
+public class p02AMinerTask {
+    public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
