cf24806bb2c566f423f09b302617a6ceffaac18b
Stela Dimitrova <stella.chilikova@gmail.com>
2022-11-29 16:56:14 +0200

    new

---
+++ TextProcessing/Lab/p04TextFilter.java
@@ -0,0 +1,12 @@
+package TextProcessing.Lab;
+
+import java.util.Scanner;
+
+public class p04TextFilter {
+    public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+
+         
+    }
+}
